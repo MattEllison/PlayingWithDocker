f161619fb86d0d465b7593c334626c61ea3fb283
Matt Ellison <matthewcellison@gmail.com>
2017-11-12 18:05:45 -0600

    First Commit

---
+++ WebApplication1_Docker/RunDocker.ps1
@@ -0,0 +1,26 @@
+﻿
+#https://docs.microsoft.com/en-us/aspnet/mvc/overview/deployment/docker-aspnetmvc
+#docker build -t mvc .
+#docker run -d --name randomanswers mvc
+
+
+docker stop matts
+docker rm matts
+
+Set-Location -Path ./WebApplication1_Docker
+docker build -t mvc . --no-cache=true
+
+
+#docker start matts
+docker run -d --name matts mvc
+
+## docker run -d --name 989a mvc
+ 
+#docker inspect d54
+
+$ipaddress = docker inspect matts -f '{{.NetworkSettings.Networks.nat.IPAddress}}'
+Write-Host $ipaddress
+
+#Start-Process  -Path "http://$ipaddress"
+
+start-process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList "http://$ipaddress"
---
+++ WebApplication1_Docker/RunDockerSQL.ps1
@@ -0,0 +1,3 @@
+﻿docker run -e "ACCEPT_EULA=Y" -e sa_password=mypassword!1 --name sql -d microsoft/mssql-server-windows-express   
+
+   docker inspect --format '{{.NetworkSettings.Networks.nat.IPAddress}}'  sql
